e7bdf61b2cb4f5ad8a06eaaf558f15c82d6723f7
gabriellyfn <gaby.gdmc@gmail.com>
2025-11-16 21:10:39 -0300

    Primeiro commit: toDoList projeto Java

---
+++ src/App.java
@@ -0,0 +1,21 @@
+//Primeiro projeto pessoal em java: To Do List!
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Tarefa { //Abaixo são os atributos da classe Tarefas
+    String aFazer;
+    boolean concluida;
+
+    //Criando um Método Construtor
+    public Tarefa(String aFazer, boolean concluida) {
+        this.aFazer = aFazer;
+        this.concluida = false; //toda tarefa começa pendente 
+    }
+}
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
