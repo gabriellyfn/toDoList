cb3b05d94d5a0c63836c1f5c59acb3c0a3ee5dea
gabriellyfn <gaby.gdmc@gmail.com>
2025-11-16 22:26:23 -0300

    Commit 2

--- src/App.java
+++ src/App.java
@@ -16,6 +16,24 @@ class Tarefa { //Abaixo são os atributos da classe Tarefas
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+        ArrayList <Tarefa> tarefas = new ArrayList<>();
+
+        int entrada = 1;
+        System.out.print("Olá, tecle (1) Iniciar o programa de Tarefas a Fazer OU (0) Sair ");
+        entrada = teclado.nextInt();
+
+        while (entrada == 1) {
+            System.out.println("\n-=-=-=-=-=TO DO LIST-=-=-=-=-=");
+            System.out.println("\n[ 1 ] Adicionar Tarefa ");
+            System.out.println("[ 2 ] Listar Tarefas ");
+            System.out.println("[ 3 ] Remover tarefa ");
+            System.out.println("[ 4 ] Sair ");
+            System.out.print("\nEscolha uma opção:");
+            int opcao = teclado.nextInt();
+
+
+        }
         
     }
 }
